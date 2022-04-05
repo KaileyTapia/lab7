6aa8cdc298328b667d43647e7b60e1c9b0d4092c
KaileyTapia <97560310+KaileyTapia@users.noreply.github.com>
2022-04-05 10:03:33 -0700

    uploaded script.js to js folder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+function init() {
+    //add your javascrip between these two lines of code
+    var userinput = document.getElementById('entryinput');
+    var btnclik = document.getElementById('entrybutton');
+    var textoutput = document.getElementById('textoutput');
+    btnclik.addEventListener('click', function() {
 
+        alert('Kailey Tapia: ' + userinput.value);
+        textoutput.innerHTML = userinput.value;
+    });
+}
+window.addEventListener('load', init);
